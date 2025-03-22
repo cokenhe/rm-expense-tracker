789325f1222c27b1476da9da4d09d5851324d28a
Ken Ho <cokenhe@gmail.com>
2025-03-22 19:04:43 -0400

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react(), vercel()],
+  plugins: [react()],
 });
