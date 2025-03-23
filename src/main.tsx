e8706bccf0d4bc6432a7ecf8086942f32e4dda51
Ken Ho <cokenhe@gmail.com>
2025-03-23 02:38:39 -0400

    chore: update config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <SpeedInsights />
+    <Analytics />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
